cf4bf1c0755d9f35f840f545c7a221a74fc67cd3
zinmyoswe <zinmyoswe@github.com>
2024-07-05 18:15:26 +0700

    add some display format and function

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -14,6 +14,8 @@ import message_icon from './message_icon.png'
 import code_icon from './code_icon.png'
 import send_icon from './send_icon.png'
 import gemini_icon from './gemini_icon.png'
+import dark_icon from './dark.png'
+import contrast_icon from './contrast.png'
 
 export const assets = {
     history_icon,
@@ -31,5 +33,7 @@ export const assets = {
     message_icon,
     code_icon,
     send_icon,
-    gemini_icon
+    gemini_icon,
+    dark_icon,
+    contrast_icon
 }
